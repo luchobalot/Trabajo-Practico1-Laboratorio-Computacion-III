7cf43ace88db1d33153c7293fd634e3f975cc3bb
luchobalot <lnbalot02@gmail.com>
2024-04-13 13:29:18 -0300

    Se sube el ejercicio 23 terminado

---
+++ TP1_Ejercicio23/src/ejercicios/ejercicio23.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+public class ejercicio23 {
+	// Se define la clase:
+	private double numero1;
+	private double numero2;
+	
+	// Constructor de la clase con los dos parametros:
+	public ejercicio23(double numero1, double numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+	
+	// Metodo que suma los dos numeros.
+	public double sumadorNumeros() {
+		return this.numero1 + this.numero2;
+	}
+	
+	public static void main(String[] args) {
+		// Se crea un objeto y se le pasan los valores de numero1 y numero2
+		ejercicio23 objeto = new ejercicio23(18.5, 76.2);
+		// Se llama al meotodo para que haga la suma
+		double suma = objeto.sumadorNumeros();
+		
+		System.out.println("La suma de los dos numeros es: "+ suma);
+	}
+
+}
